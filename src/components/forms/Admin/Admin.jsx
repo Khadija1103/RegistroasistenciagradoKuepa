da090b809dece77345148509ead94dc93897937d
Aura_Mejia <aurasofiamejia2805@gmail.com>
2021-07-27 21:25:05 -0500

    Revisión del vídeo de conexión con firebase en el componente del formulario de administrativo

--- src/components/forms/Admin/Admin.jsx
+++ src/components/forms/Admin/Admin.jsx
@@ -1,10 +1,33 @@
-import React from 'react' /*Import React*/ 
+import React, {useState} from "react" /*import react y crear el estado de la aplicación que pertenece a un componente*/ 
 import '../css/Admin.css'/*Import the styles*/
 import Birrete from '../../../assets/images/birrete.png' 
 import Diploma from '../../../assets/diploma.png'
 
 
-function Admin () {
+const Admin = (props) => { /*Traer de link*/
+
+    const initialSateValues = { /*Guardar todo en una constante con valores iniciales*/
+        nombre_estudiante: '',
+        nombre:'',
+        cargo:'',
+        tel: '',
+        apellido: '',
+        certify:'',
+    };
+
+    const [values, setValues] = useState(initialSateValues) /*Definir el estado desde React, el set adquiere su nombre por el valor actual de la función*/
+
+    const handleInputChange = e => { /*Captura los datos*/
+        const {name,value} = e.target; /*Me va a mostrar esos campos específicos dentro de llaves*/
+        setValues({...values, [name]: value}) /*Copie lo que actualmente tiene y con el input que está actualziando coloca ese input*/
+        
+    }
+
+    const handlesubmit = e => {
+        e.preventDefault();
+        props.addOrEditLink(values);
+        setValues({...initialSateValues})/*Guarda los datos y los devuelve al estado inicial a través del value en el input*/
+    }
     return (
         <div className="Container_Admin"> 
 
@@ -15,21 +38,21 @@ function Admin () {
                         ADMINISTRATIVO
                     </h1>
 
-                    <form action="">
+                    <form action="" onSubmit={handleSubmit}>
                         <p>
-                             <input type="text" name="nya" id="nombre_estudiante" value="" placeholder="Nombre del estudiante" />
+                             <input type="text" name="nombre_estudiante" id="nombre_estudiante" onChange={handleInputChange} value={values.nombre_estudiante} placeholder="Nombre del estudiante" />
                        </p>
                        <p>
-                           <input type="text" name="nya" id="nombre" value="" placeholder="Nombre" />
+                           <input type="text" name="nombre" id="nombre" onChange={handleInputChange} value={values.nombre} placeholder="Nombre" />
                        </p>
                        <p>
-                           <input type="text" name="nya" id="apellido" placeholder="Apellido"/>
+                           <input type="text" name="apellido" id="apellido" onChange={handleInputChange} value={values.apellido} placeholder="Apellido"/>
                        </p>
                        <p>
-                           <input type="number" name="nya" id="tel" placeholder="Teléfono"/>
+                           <input type="number" name="tel" id="tel" onChange={handleInputChange} value={values.tel} placeholder="Teléfono"/>
                        </p>
                        <p>
-                           <input type="text" name="nya" id="cargo" placeholder="Cargo"/>
+                           <input type="text" name="cargo" id="cargo"onChange={handleInputChange} value={values.cargo} placeholder="Cargo"/>
                        </p>
                        
 
@@ -38,8 +61,8 @@ function Admin () {
 
                        </p>
                        <p>
-                           Si <input type="radio" value="certify" id="yes" checked/> 
-                           No <input type="radio" value="certify" id="no" checked/> 
+                           Si <input type="radio" value="certify" onChange={handleInputChange} id="yes" value={values.certify} checked/> 
+                           No <input type="radio" value="certify" onChange={handleInputChange} id="no" value={values.certify} checked/> 
                        </p>
                        <p>
                            <input type="submit" value="Enviar" />
