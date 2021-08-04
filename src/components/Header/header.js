db939aa16a31cc88ee09dc2cee85ad3bccaa8c0d
Aura_Mejia <aurasofiamejia2805@gmail.com>
2021-08-04 09:42:44 -0500

    Estilos de los forms al 90%

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,7 +1,10 @@
 
 import logo from "../../assets/images/logo.png";
-
-import './header.css';
+import './header.css'
+import City from'../../Utils/Img/Logos/logo-citi-foundation.png';
+import Global from'../../Utils/Img/Logos/global.png';
+import Futuro from'../../Utils/Img/Logos/futuro.png';
+import Kuepa from'../../Utils/Img/Logos/kuepa.png';
 
 
 function Header() {
@@ -9,7 +12,10 @@ function Header() {
 
    <div className="cajaheader">
     <div className="logo">
-    <img className="img-fluid" src={logo} />   
+    <img id="img2" src={City} alt="Esta es la img2" />
+    <img id="img2" src={Global} alt="Esta es la img2" />
+    <img id="img2" src={Futuro} alt="Esta es la img2" />
+    <img id="img2" src={Kuepa} alt="Esta es la img2" />
     </div>
     </div>
   );
